118709f4cffd5a215c07722c6df327e1a125b37d
Sudhirarya <sudhirarya7033@gmail.com>
2024-07-13 12:46:51 +0530

    initial commit

---
+++ src/main/java/emailApp/emailApp/emailApp.java
@@ -0,0 +1,10 @@
+package emailApp.emailApp;
+
+public class emailApp {
+
+	public static void main(String args[]) {
+		
+		email eml = new email("sudhir"," kumar ");
+          System.out.println(eml.showInfo());
+	}
+}
